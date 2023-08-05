57f49bff392a6dc9f007129046319d94dee4945c
Ryan J. Williams <ryanjacobwilliams@gmail.com>
2023-08-05 12:04:15 -0500

    chore: add prettier, eslint, and initial layout skeleton

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { Text, SafeAreaView, StatusBar, StyleSheet, View } from 'react-native';
+import React from 'react';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <ExpoStatusBar style="auto" />
+      <SafeAreaView style={styles.container}>
+        <View style={styles.search}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.list}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
+    flex: 1,
     justifyContent: 'center',
+    marginBottom: StatusBar.currentHeight,
+    marginTop: StatusBar.currentHeight,
+  },
+  list: {
+    backgroundColor: 'blue',
+    flex: 1,
+    padding: 12,
+    width: '100%',
+  },
+  search: {
+    backgroundColor: 'green',
+    justifyContent: 'center',
+    padding: 12,
+    width: '100%',
   },
 });
